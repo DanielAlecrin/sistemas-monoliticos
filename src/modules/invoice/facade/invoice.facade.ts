b9e5fe4b258a7876c382b44ffc964188f7e0830e
Daniel Alecrin <daniel.alecrin@totvs.com.br>
2024-03-28 17:52:32 -0300

    feat endpoints product client checkout invoice

--- src/modules/invoice/facade/invoice.facade.ts
+++ src/modules/invoice/facade/invoice.facade.ts
@@ -3,6 +3,7 @@ import InvoiceFacadeInterface, {
   FindInvoiceFacadeInputDto,
   FindInvoiceFacadeOutputDto,
   GenerateInvoiceFacadeInputDto,
+  GenerateInvoiceFacadeOutputDto,
 } from "./invoice.facade.interface";
 
 export interface UseCaseProps {
@@ -18,8 +19,10 @@ export default class InvoiceFacade implements InvoiceFacadeInterface {
     this._generateUsecase = usecaseProps.generateUsecase;
   }
 
-  async generate(input: GenerateInvoiceFacadeInputDto): Promise<void> {
-    await this._generateUsecase.execute(input);
+  async generate(
+    input: GenerateInvoiceFacadeInputDto
+  ): Promise<GenerateInvoiceFacadeOutputDto> {
+    return await this._generateUsecase.execute(input);
   }
 
   async find(
